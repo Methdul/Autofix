47d5168df8d2718d6ffb0d26e3740fc7567b51e1
MKM Waseem <waseem.20232722@iit.ac.lk>
2026-03-18 18:23:19 +0530

    chore: initial component setup and lucide-react imports

---
+++ client/src/components/dashboard/owner/vehicles/VehicleSpecsSection.tsx
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+import { Car, Calendar, Hash, CheckCircle2, Tractor as Chassis, Save, X } from 'lucide-react';
+import type { Vehicle } from '../../../../api/vehicle.api';
